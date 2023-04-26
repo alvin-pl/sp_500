70f6df1fa2641bd1c27b1d33230ed3ef59b22428
alvinpl <alvinpl21@gmail.com>
2023-04-26 19:28:46 -0400

    new changes

--- alpaca_keys.py
+++
@@ -1,5 +0,0 @@
-API_KEY = 'PKY5N229HKHKK3C3GNWZ'
-API_SECRET = 'uoHW8qGQ6ldWdbgDUqya2SXbmCLeBc8t5DGCFqwc'
-#Base URL is all that's really needed
-BASE_URL = "https://paper-api.alpaca.markets"
-#BASE_URL = 'https://data.sandbox.alpaca.markets/v2'
