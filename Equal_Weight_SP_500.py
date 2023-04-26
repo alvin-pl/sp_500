94fc20c6e674fbfcf92645cc27c188cc70d4bfe0
alvinpl <alvinpl21@gmail.com>
2023-04-26 17:58:31 -0400

    pushed and committed pending changes

--- Equal_Weight_SP_500.py
+++ Equal_Weight_SP_500.py
@@ -27,27 +27,24 @@ stocks = pd.read_csv('/Users/alvinpl/Desktop/sp_500/sp_500_stocks.csv')
 #acquire API token
 alpaca = api.REST(API_KEY, API_SECRET, BASE_URL)
 
-
-#Access data for the stock symbols
-stock_client = StockHistoricalDataClient(API_KEY, API_SECRET)
-
-multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["AMZN"]) #"NVDA", "UBER"
-latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
-print('Stock Symbol Data')
-print(latest_multisymbol_quotes['ask_price'])
-
 #Make our first API call
 #authenticate trading client
 trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
-#Using web-socket API to stream real time data
-# wss_client = StockDataStream(API_KEY, API_SECRET)
 
-# async def quote_data_handler(data):
-#     #print trade data, get the size and price
-#     print(data)
+stock_client = StockHistoricalDataClient(API_KEY, API_SECRET)
+
+# multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["AMZN", "NVDA", "UBER"])
+# latest_multisymbol_quotes = stock_client.get_stock_latest_quote(multisymbol_request_params)
+# print(latest_multisymbol_quotes)
+
+#Using web-socket API to stream real time data
+wss_client = StockDataStream(API_KEY, API_SECRET)
+
+async def quote_data_handler(data):
+    print(data)
     
-# wss_client.subscribe_trades(quote_data_handler, 'AMZN')
-# wss_client.run()
+wss_client.subscribe_trades(quote_data_handler, 'AMZN')
+wss_client.run()
 
 
 
